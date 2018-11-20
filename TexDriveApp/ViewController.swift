bc687b1087f1f957128f0dbd4c191d1f52e172cd
Erwan Masson <erwan.masson.comet@axa.com>
2018-11-20 16:32:23 +0100

    [V0] Add some Info on ViewController [InfoPlist] Change Version

--- TexDriveApp/ViewController.swift
+++ TexDriveApp/ViewController.swift
@@ -11,21 +11,49 @@ import CoreLocation
 import TexDriveSDK
 import CallKit
 import CoreMotion
+import Crashlytics
+import RxSwift
+import os
 
 class ViewController: UIViewController {
     @IBOutlet weak var TripSegmentedControl: UISegmentedControl!
     
+    @IBOutlet weak var logTextField: UITextView!
+    @IBOutlet weak var textfield: UITextField!
     var tripRecorder : TripRecorder?
     var locationManager = CLLocationManager()
+    let rx_disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        TripSegmentedControl.selectedSegmentIndex = 1
         locationManager.requestAlwaysAuthorization()
-        let user = User.Anonymous
+    }
+    
+    @IBAction func tripSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            tripRecorder?.stop()
+            launchTracking()
+            tripRecorder?.start()
+            break
+        default:
+            tripRecorder?.stop()
+        }
+    }
+    
+    func launchTracking()  {
+        var user = User.Anonymous
+        
+        if let userName = textfield.text {
+            user = User.Authentified(userName)
+            self.logUser(userName: userName)
+        }
+        
         do {
             if let configuration = try Config(applicationId: "APP-TEST", applicationLocale: Locale.current, currentUser: user, currentMode: Mode.manual) {
                 tripRecorder = TripRecorder(config: configuration)
-                tripRecorder!.start()
+                configureLog(configuration.rx_log)
             }
         } catch ConfigurationError.LocationNotDetermined(let description) {
             print(description)
@@ -33,15 +61,53 @@ class ViewController: UIViewController {
             print("\(error)")
         }
     }
-
-    @IBAction func tripSegmentedControlValueChanged(_ sender: UISegmentedControl) {
-        switch sender.selectedSegmentIndex {
-        case 0:
-            tripRecorder?.start()
+    
+    func configureLog(_ log: PublishSubject<LogDetail>) {
+        log.asObservable().observeOn(MainScheduler.asyncInstance).subscribe { [weak self](event) in
+            if let logDetail = event.element {
+                self?.report(logDetail: logDetail)
+            }
+            }.disposed(by: self.rx_disposeBag)
+        
+    }
+    
+    func report(logDetail: LogDetail) {
+        let customLog = OSLog(subsystem: "fr.axa.tex", category: logDetail.fileName)
+        
+        switch logDetail.type {
+        case .Info:
+            Answers.logCustomEvent(withName: logDetail.functionName,
+                                           customAttributes: [
+                                            "filename" : logDetail.fileName,
+                                            "functionName": logDetail.functionName,
+                                            "type": "Info",
+                                            "detail": logDetail.detail
+                ])
+            os_log("%@", log: customLog, type: .info, logDetail.description)
             break
-        default:
-            tripRecorder?.stop()
+        case .Warning:
+            Crashlytics.sharedInstance().recordError(NSError(domain: logDetail.fileName, code: 1111, userInfo: ["filename" : logDetail.fileName, "functionName": logDetail.functionName, "description": logDetail.detail]))
+            os_log("%@", log: customLog, type: .debug, logDetail.description)
+            break
+        case .Error:
+            os_log("%@", log: customLog, type: .error, logDetail.description)
+            Crashlytics.sharedInstance().recordError(NSError(domain: logDetail.fileName, code: 9999, userInfo: ["filename" : logDetail.fileName, "functionName": logDetail.functionName, "description": logDetail.detail]))
+            break
         }
+        let oldLog = self.logTextField.text ?? ""
+        let newLog = String(describing:logDetail.description)
+        let text = "\(oldLog)\n\(newLog)"
+        print("oldLog"+oldLog)
+        print("newLog"+newLog)
+        self.logTextField.text = text
+        
+        print(text)
+    }
+    
+    func logUser(userName: String) {
+        Crashlytics.sharedInstance().setUserIdentifier(userName)
+        Crashlytics.sharedInstance().setUserName(userName)
     }
 }
+
 
