3580213dbedf28bd220e393412ecf8b92a9de4b3
Erwan Masson <erwan.masson.comet@axa.com>
2019-01-21 11:41:53 +0100

    [PersistantApp] First version of a tool to be always persistent

---
+++ TexDriveSDK/TexDriveSDK/Auto-Mode/PersistantApp.swift
@@ -0,0 +1,35 @@
+//
+//  PersistantApp.swift
+//  TexDriveSDK
+//
+//  Created by Erwan Masson on 21/01/2019.
+//  Copyright © 2019 Axa. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class PersistantApp: NSObject, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.pausesLocationUpdatesAutomatically = false
+        locationManager.activityType = .automotiveNavigation
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.startMonitoringSignificantLocationChanges()
+        
+    }
+    // MARK : CLLocationManagerDelegate
+    private func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        Log.print("didUpdateLocations")
+    }
+    
+    private func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        Log.print("didFailWithError", type: .Error)
+    }
+}
